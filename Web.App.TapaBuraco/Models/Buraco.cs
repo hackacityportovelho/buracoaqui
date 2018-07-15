615e2312d0395c6d1614d08154fd6309391cadba
Hudson Fernando <gtisupel@gmail.com>
2018-07-14 23:31:50 -0400

    confirmação da img

---
+++ Web.App.TapaBuraco/Models/Buraco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.App.TapaBuraco.Models
+{
+    public class Buraco
+    {
+        public Buraco()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string Rua { get; set; }
+        public string Bairro { get; set; }
+    }
+}
