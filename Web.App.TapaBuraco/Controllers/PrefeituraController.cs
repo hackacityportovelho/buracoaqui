3e53024f5d5d3f981f04c1856c4194aa0e7b11e8
Hudson Fernando <buracoaquipvh@gmail.com>
2018-07-15 02:12:08 -0400

    Cadastro prefeitura

--- Web.App.TapaBuraco/Controllers/PrefeituraController.cs
+++ Web.App.TapaBuraco/Controllers/PrefeituraController.cs
@@ -1,13 +1,36 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using Web.App.TapaBuraco.Models;
 
 namespace Web.App.TapaBuraco.Controllers
 {
     public class PrefeituraController : Controller
     {
+        private Contexto dbContexto = new Contexto();
+
         // GET: Prefeitura
         public ActionResult Cadastro()
         {
+
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Cadastro(Prefeitura prefeitura)
+        {
+            try
+            {
+                dbContexto.Prefeitura.Add(prefeitura);
+                dbContexto.SaveChanges();
+                return RedirectToAction("Index","Home");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return View("Cadastro", prefeitura);
+            }
+        }
+
+
     }
 }
