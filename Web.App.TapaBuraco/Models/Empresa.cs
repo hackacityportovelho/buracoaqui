2781a1c4bc43c288bc040ef524093c11d48abb0d
Hudson Fernando <hudsonhfmf@gmail.com>
2018-07-14 23:59:33 -0400

    entity

--- Web.App.TapaBuraco/Models/Empresa.cs
+++ Web.App.TapaBuraco/Models/Empresa.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Web.App.TapaBuraco.Models
 {
     public class Empresa
     {
+        [DisplayName("Razão sócial")]
         public string RazaoSocial { get; set; }
+        [DisplayName("CNPJ")]
         public string Cnpj { get; set; }
+        [DisplayName("NIRE")]
         public string Nire { get; set; }
-        public string Enderco { get; set; }
+        [DisplayName("Endereço")]
+        public string Endereco { get; set; }
+        [DisplayName("Número")]
         public string Numero { get; set; }
+        [DisplayName("Bairro")]
         public string Bairro { get; set; }
+        [DisplayName("CEP")]
+        public string Cep { get; set; }
 
         public ICollection<Representante> RepresentantecCollection { get; set; }
     }
