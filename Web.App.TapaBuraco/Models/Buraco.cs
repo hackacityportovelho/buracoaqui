82bc5c78f4a3a05f4b5d6754a7ac101c16ab62a3
Hudson Fernando <gtisupel@gmail.com>
2018-07-14 23:59:54 -0400

    Tela de cadastro de buraco

--- Web.App.TapaBuraco/Models/Buraco.cs
+++ Web.App.TapaBuraco/Models/Buraco.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,9 +13,14 @@ namespace Web.App.TapaBuraco.Models
             Id = Guid.NewGuid();
         }
         public Guid Id { get; set; }
+        [Display(Name = "Descrição: (Opcional)")]
         public string Descricao { get; set; }
         public DateTime DataCadastro { get; set; }
+
+        [Required(ErrorMessage = "Informe a rua")]
+        [Display(Name = "Rua: ()")]
         public string Rua { get; set; }
+        [Required(ErrorMessage = "Informe o bairro")]
         public string Bairro { get; set; }
     }
 }
