3ea08ec899484db890013c6b27194f51b6b3869b
Hudson Fernando <hudsonhfmf@gmail.com>
2018-07-14 23:26:16 -0400

    Criação das classes empresa e representante

---
+++ Web.App.TapaBuraco/Models/Empresa.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Web.App.TapaBuraco.Models
+{
+    public class Empresa
+    {
+        public string RazaoSocial { get; set; }
+        public string Cnpj { get; set; }
+        public string Nire { get; set; }
+        public string Enderco { get; set; }
+        public string Numero { get; set; }
+        public string Bairro { get; set; }
+
+        public ICollection<Representante> RepresentantecCollection { get; set; }
+    }
+}
---
+++ Web.App.TapaBuraco/Models/Representante.cs
@@ -0,0 +1,14 @@
+﻿namespace Web.App.TapaBuraco.Models
+{
+    public class Representante
+    {
+        public string Cargo { get; set; }
+        public string Nome { get; set; }
+        public string Naturidade { get; set; }
+        public string EstadoCivil { get; set; }
+        public string Profissao { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public Empresa Empresa { get; set; }
+    }
+}
